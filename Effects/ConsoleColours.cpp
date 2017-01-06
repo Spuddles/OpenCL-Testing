f6751e0fbb1f31c8f2a22ac87cfd19210154f021
Spuddles <Spuddles666@gmail.com>
2017-01-06 10:12:56 +0000

    Implemented console colour effect

--- Effects/ConsoleColours.cpp
+++ Effects/ConsoleColours.cpp
@@ -18,12 +18,29 @@ bool ConsoleColours::run(RGBA *input, RGBA *output)
 	for (int i = 0; i < 640 * 400; i++)
 	{
 		RGBA rgba = (*input++);
+		int offset = -1;
+		float closest = 10e15;
 
+		// Look for the closest colour in 3D space
+		for (int i=0;i<vecColours.size();++i)
+		{
+			RGBA col = vecColours[i];
 
+			int dr = rgba.R - col.R;
+			int dg = rgba.G - col.G;
+			int db = rgba.B - col.B;
 
+			float dist = (dr*dr) + (dg*dg) + (db*db);
 
+			if (dist < closest)
+			{
+				offset = i;
+				closest = dist;
+			}
+		}
 
-		*(output++) = rgba;
+		// Write out the console RGB version
+		*(output++) = vecColours[offset];
 	}
 	return true;
 }
