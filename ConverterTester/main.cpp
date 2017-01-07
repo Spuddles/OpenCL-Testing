7859643224ab48dbeea9e8e44bbcf3d7636aad40
Spuddles <Spuddles666@gmail.com>
2017-01-07 11:40:33 +0000

    Added utils project to build

--- ConverterTester/main.cpp
+++ ConverterTester/main.cpp
@@ -1,6 +1,39 @@
 #include "convertertester.h"
 #include <QtWidgets/QApplication>
 
+#include "../Utils/Utils.h"
+
+/*
+ * Function to turn the 16*16 tile charset into a 1*256 version for easier use
+void temp()
+{
+	// Load the charset bitmap
+	char *input = Utils::loadBinaryFile("..\\Images\\Charset.data");
+
+	// Write it out in the char order
+	char output[8*8*256] = { 0 };
+
+	char* outputPtr = &output[0];
+	for (int a = 0; a < 16; a++)
+	{
+		for (int b = 0; b < 16; b++)
+		{
+			// Find the start of the char we want to extract
+			char* temp = input + (a * 16 * 8 * 8) + (b * 8);
+			for (int y = 0; y < 8; y++)
+			{
+				for (int x = 0; x < 8; x++)
+				{
+					*(outputPtr++) = *(temp++);
+				}
+				temp += (15 * 8);
+			}
+		}
+	}
+
+	Utils::saveBinaryFile("..\\Images\\Charset-ordered.data", (char*)&output, sizeof(output));
+}*/
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
--- Converters/Nearest.cpp
+++ Converters/Nearest.cpp
@@ -17,8 +17,11 @@ bool Nearest::initialise()
 
 bool Nearest::convert(RGBA *input, CHAR_INFO *output)
 {
-	for (int i = 0; i < 640 * 400; i++)
+	for (int i = 0; i < 80*50; i++)
 	{
+		// First extract the char we want to process
+
+		// Now look for the closest char to it
 		RGBA rgba = (*input++);
 		output->Char.AsciiChar = '*';
 		output++;
